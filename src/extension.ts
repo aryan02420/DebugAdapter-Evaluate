91f8e91687691b11392da9812603b1046c78ea13
aryan02420 <aryan02420@gmail.com>
2022-06-19 13:08:17 +0000

    feat: evaluate string from user

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const commands = [
+	{
+		id: 'debugadapter-evaluate.evaluate',
+		handler: async () => {
+			const debugSession = vscode.debug.activeDebugSession;
+			if (!debugSession) {
+				vscode.window.showErrorMessage('No active debug session. Start debugging first.');
+				return;
+			}
+
+			const inputOptions: vscode.InputBoxOptions = {
+				prompt: 'Expression',
+				title: 'Execute in Debugger'
+			};
+			const inputExpression = (await vscode.window.showInputBox(inputOptions))?.trim();
+			if (!inputExpression) {
+				return;
+			}
+
+			vscode.debug.activeDebugConsole.appendLine(`→ ${inputExpression}`);
+
+			// https://microsoft.github.io/debug-adapter-protocol/specification#Requests_Evaluate
+			const evaluateArgs = {
+				expression: inputExpression,
+				context: 'repl',
+			};
+			const { result: evalResult } = await debugSession.customRequest('evaluate', evaluateArgs);
+			vscode.debug.activeDebugConsole.appendLine(`← ${evalResult}`);
+		}
+	},
+];
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "debugadapter-evaluate" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('debugadapter-evaluate.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DebugAdapter Evaluate!');
-	});
-
-	context.subscriptions.push(disposable);
+	for (const {id, handler} of commands) {
+		context.subscriptions.push(vscode.commands.registerCommand(id, handler));
+	}
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
